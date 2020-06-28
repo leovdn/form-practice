fea85a8eb3e3b276efac3c1b419e60a4be1649b8
leovdn <leo_vdn@hotmail.com>
2020-06-27 22:44:54 -0300

    adjusted styles and built javascript validation structure

---
+++ main.js
@@ -0,0 +1,38 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    checkInputs();
+})
+
+function checkInputs() {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+
+    if(usernameValue === '') {
+        setErrorFor(username, 'Este campo deve ser preenchido');
+    } else {
+        setSuccesFor(username);
+    }
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+
+    small.innerText = message;
+    formControl.className = 'form-control error';
+}
+
+function setSuccesFor(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+
+}
